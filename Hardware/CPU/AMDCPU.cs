fbbb8c391b4b6fe28ebfc7648b4761621e986316
Aitor González Fernández <reimashi@gmail.com>
2016-02-07 02:15:34 +0100

    Added support to detect some AMD microarchitectures

--- Hardware/CPU/AMDCPU.cs
+++ Hardware/CPU/AMDCPU.cs
@@ -8,35 +8,109 @@
 	
 */
 
-namespace Jotai.Hardware.CPU {
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
-  internal abstract class AMDCPU : GenericCPU {
+namespace Jotai.Hardware.CPU
+{
 
-    private const byte PCI_BUS = 0;
-    private const byte PCI_BASE_DEVICE = 0x18;
-    private const byte DEVICE_VENDOR_ID_REGISTER = 0;
-    private const ushort AMD_VENDOR_ID = 0x1022;
+    [JsonObject(MemberSerialization.OptIn)]
+    public abstract class AMDCPU : GenericCPU
+    {
 
-    public AMDCPU(int processorIndex, CPUID[][] cpuid, ISettings settings)
-      : base(processorIndex, cpuid, settings) { }
+        public enum Microarchitecture
+        {
+            Unknown,
+            K5,
+            K6,
+            K6_2,
+            Sharptooth,
+            Athlon,
+            Hammer,
+            K10,
+            Fusion,
+            Bobcat,
+            Jaguar,
+            Puma,
+            Bulldozer,
+            Piledriver,
+            Steamroller,
+            Excavator,
+            Zen
+        }
 
-    protected uint GetPciAddress(byte function, ushort deviceId) {
-      
-      // assemble the pci address
-      uint address = Ring0.GetPciAddress(PCI_BUS,
-        (byte)(PCI_BASE_DEVICE + processorIndex), function);
+        private const byte PCI_BUS = 0;
+        private const byte PCI_BASE_DEVICE = 0x18;
+        private const byte DEVICE_VENDOR_ID_REGISTER = 0;
+        private const ushort AMD_VENDOR_ID = 0x1022;
 
-      // verify that we have the correct bus, device and function
-      uint deviceVendor;
-      if (!Ring0.ReadPciConfig(
-        address, DEVICE_VENDOR_ID_REGISTER, out deviceVendor))
-        return Ring0.InvalidPciAddress;
-      
-      if (deviceVendor != (deviceId << 16 | AMD_VENDOR_ID))
-        return Ring0.InvalidPciAddress;
+        private readonly Microarchitecture microarchitecture;
 
-      return address;
+        [JsonProperty]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Microarchitecture MicroArchitecture { get { return this.microarchitecture; } }
+
+        public AMDCPU(int processorIndex, CPUID[][] cpuid, ISettings settings) : base(processorIndex, cpuid, settings)
+        {
+            this.microarchitecture = Microarchitecture.Unknown;
+            System.Console.WriteLine(this.family);
+            switch (this.family)
+            {
+                case 0xC:
+                    this.microarchitecture = Microarchitecture.K10;
+                    break;
+                case 0xE:
+                    this.microarchitecture = Microarchitecture.Bobcat;
+                    break;
+                case 0x10:
+                    switch (this.model)
+                    {
+                        case 0x2:
+                            this.microarchitecture = Microarchitecture.Jaguar;
+                            break;
+                        case 0x3:
+                            this.microarchitecture = Microarchitecture.Puma;
+                            break;
+                    }
+                    break;
+                case 0x15:
+                    switch (this.model)
+                    {
+                        case 0x1:
+                            this.microarchitecture = Microarchitecture.Bulldozer;
+                            break;
+                        case 0x2:
+                            this.microarchitecture = Microarchitecture.Piledriver;
+                            break;
+                        case 0x3:
+                            this.microarchitecture = Microarchitecture.Steamroller;
+                            break;
+                        case 0x4:
+                            this.microarchitecture = Microarchitecture.Excavator;
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        protected uint GetPciAddress(byte function, ushort deviceId)
+        {
+
+            // assemble the pci address
+            uint address = Ring0.GetPciAddress(PCI_BUS,
+              (byte)(PCI_BASE_DEVICE + processorIndex), function);
+
+            // verify that we have the correct bus, device and function
+            uint deviceVendor;
+            if (!Ring0.ReadPciConfig(
+              address, DEVICE_VENDOR_ID_REGISTER, out deviceVendor))
+                return Ring0.InvalidPciAddress;
+
+            if (deviceVendor != (deviceId << 16 | AMD_VENDOR_ID))
+                return Ring0.InvalidPciAddress;
+
+            return address;
+        }
+
     }
-
-  }
 }
